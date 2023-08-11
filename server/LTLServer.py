585545a8392a3fe09e3ffc7f689e1b9c029e17a6
Alyssa2026 <Alyssasun2022@gmail.com>
2023-08-10 22:07:37 -0700

    added LTL

--- server/LTLServer.py
+++ server/LTLServer.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-import requests
 import xml.etree.ElementTree as ET
-import re
 from string import digits
 from flask_cors import CORS, cross_origin
+import Lang2LTL
+import Lang2LTL.lang2ltl
+
 
 app = Flask(__name__)
 CORS(app)
@@ -19,14 +20,16 @@ def convertLTL():
    # Read the first line of the JSON file
    # first_line = ''
    #with open(file, 'r') as json_file:
-   #     first_line = json_file.readline()
+   #first_line = json_file.readline()impo
+   # out = Lang2LTL.lang2ltl.lang2ltl(user_input, file, 3)
 
 
    # return the user input 
    # return jsonify(user_input)
    # return the file 
    # return jsonify(file)
-   return 'G(Slater Hall -> X(Salomon Center For Teaching))'
+
+   return jsonify('G(Slater Hall -> X(Salomon Center For Teaching))')
    
    
 if __name__ == '__main__':
